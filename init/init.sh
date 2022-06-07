9923103ae411dd18132b004596064020522bdd4a
Beau Berger <96446663+beau-ronin@users.noreply.github.com>
2022-06-07 11:20:26 -0500

    drop database whenever running initialization code. (#67)

--- init/init.sh
+++ init/init.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+mysql -h mockehrmysql -P 3306 -u springuser -pThePassword <<MY_QUERY
+DROP DATABASE IF EXISTS mock_ehr_db;
+CREATE DATABASE mock_ehr_db;
+MY_QUERY
 for a in /init/*; do
   for b in $a/*; do
     for c in $b/*.json; do
