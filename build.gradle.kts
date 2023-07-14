0d66ffd6d3cc5293d4aa0ad8388220d355a12535
Josh Smith <43484632+arssycro@users.noreply.github.com>
2023-07-14 13:39:14 -0500

    Fix bootJar (#337)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.interop.gradle.junit) apply false
     alias(libs.plugins.interop.gradle.server.publish) apply false
     alias(libs.plugins.interop.gradle.spring) apply false
-    alias(libs.plugins.interop.gradle.spring.boot)
+    alias(libs.plugins.interop.gradle.spring.boot) apply false
     alias(libs.plugins.interop.gradle.server.version)
     alias(libs.plugins.interop.version.catalog)
 }
@@ -11,7 +11,4 @@ subprojects {
     // The task filter below does not appear to fire unless we include something being applied.
     // In other builds, it's publish. But we're not using publish in all subprojects, so I've used base.
     apply(plugin = "com.projectronin.interop.gradle.base")
-
-    // Disable releases hub from running on the subprojects. Main project will handle it all.
-    tasks.filter { it.group.equals("releases hub", ignoreCase = true) }.forEach { it.enabled = false }
 }
